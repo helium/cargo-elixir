b8e8e991e6006b62cd44a2fc2d5af6d4c03d6fb7
victor <victor@helium.com>
2020-10-29 14:16:10 -0700

    update versions

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -23,9 +23,17 @@ secret_key_base =
     environment variable SECRET_KEY_BASE is missing.
     You can generate one by calling: mix phx.gen.secret
     """
-
 config :cargo_elixir, CargoElixirWeb.Endpoint,
+  url: [scheme: "https", host: "cargo.helium.com", port: 443],
   http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
+  check_origin: false,
+  # check_origin: [
+  #   "https://cargo-elixir.herokuapp.com",
+  #   "https://cargo.helium.com",
+  # ],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: secret_key_base
+  
 
 config :cargo_elixir, console_stats_secret: System.get_env("CONSOLE_STATS_SECRET")
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule CargoElixir.MixProject do
     [
       app: :cargo_elixir,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.9.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
@@ -43,6 +43,7 @@ defmodule CargoElixir.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
+      {:cowboy, "~> 2.7.0"},
       {:httpoison, "~> 1.6"},
       {:cors_plug, "~> 2.0"},
     ]
