5b702aeff998abd4f602723fc73a51a7f2e54771
victor <victor@helium.com>
2020-10-30 14:51:57 -0700

    Docker

--- config/prod.exs
+++ config/prod.exs
@@ -59,4 +59,16 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs which loads secrets
 # and configuration from environment variables.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
+
+config :cargo_elixir, CargoElixirWeb.Endpoint,
+  url: [host: "example.com", port: 80],
+  cache_static_manifest: "priv/static/cache_manifest.json"
+# Do not print debug messages in production
+
+# ## Using releases (Elixir v1.9+)
+#
+# If you are doing OTP releases, you need to instruct Phoenix
+# to start each relevant endpoint:
+#
+config :cargo_elixir, CargoElixirWeb.Endpoint, server: true
